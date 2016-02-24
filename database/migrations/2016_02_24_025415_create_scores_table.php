0252479238cb79044942eb318675cb5499ec82b2
Cosmitar <mhherrera31@gmail.com>
2016-02-24 01:03:02 -0300

    add migrato for scores table

---
+++ database/migrations/2016_02_24_025415_create_scores_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('scores', function(Blueprint $table)
+        {
+            $table->integer('user_id')->unsigned();
+            $table->string('genre');
+            $table->integer('points');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('scores', function (Blueprint $table) {
+            $table->dropForeign('scores_user_id_foreign');
+        });
+
+        Schema::drop('scores');
+    }
+}
